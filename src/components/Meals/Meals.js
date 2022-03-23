387099e678792026e3d8106be7f0dfc02aa7c5d5
H_A _Opu <wwwhasib4@gmail.com>
2022-03-24 01:32:13 +0600

    added some style at header

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from "react";
+import "./Meals.css";
+const Meals = () => {
+  // const [meals,setMeals] = useState([])
+  // useEffect(()=>{
+
+  // },[])
+  return <div></div>;
+};
+
+export default Meals;
